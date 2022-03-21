f30171ae7b84d3c171b3efed0ca39c6d765b287b
michaeldeven <michael.deven1999@gmail.com>
2022-03-21 16:12:54 +0700

    revisiii

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -1,11 +1,44 @@
 package service
 
 import (
+	"context"
 	"database/sql"
+	"deven/api/helper"
+	"deven/api/model/web"
 	"deven/api/repository"
 )
 
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+}
+
+func (servoce *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	tx, err := service.DB.Begin()
+	helper.PanicIfError(err)
+	defer func() {
+		err := recover()
+		if err != nil {
+			tx.Rollback()
+			panic(err)
+		} else {
+			tx.Commit()
+		}
+	}()
+}
+
+func (servoce *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	panic("implement me")
+}
+
+func (servoce *CategoryServiceImpl) Delete(ctx context.Context, categoryId int)  {
+	panic("implement me")
+}
+
+func (servoce *CategoryServiceImpl) FindById(ctx context.Context, categoryId int) web.CategoryResponse {
+	panic("implement me")
+}
+
+func (servoce *CategoryServiceImpl) FindAll(ctx context.Context) []web.CategoryResponse {
+	panic("implement me")
 }
