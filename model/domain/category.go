dcaef15b3157f7e28ce487c0f6e76f95bb9d6e9e
michaeldeven <michael.deven1999@gmail.com>
2022-03-21 09:38:29 +0700

    menambahkan model-domain, repository

---
+++ model/domain/category.go
@@ -0,0 +1,7 @@
+package domain
+
+type Category struct {
+	Id int 
+	Name string
+}
+
---
+++ repository/category_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "context"
+
+type CategoryRepository interface {
+	Save(ctx context.Context, sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, sql.Tx, category domain.Category) 
+	FindById(ctx context.Context, sql.Tx, category Id int) domain.Category
+	FindAll(ctx context.Context, sql.Tx) []
+}
